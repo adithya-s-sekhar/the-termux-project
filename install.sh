549ec33a85c8d756cd17e61fe23d483f7dc28682
adi <adithya0332@gmail.com>
2020-05-09 19:06:17 +0530

    Unified all setup scripts as one install.sh file

--- install.sh
+++ install.sh
@@ -3,11 +3,54 @@
 # Initial Date: 09/May/2020
 # Current Date: 09/May/2020
 
-# Install packages
-sh setup-pkgs.sh
+# Update repos and upgrade packages
+apt update
+apt upgrade -y
 
-# Install scripts
-sh setup-scripts.sh
+# Install wget, ffmpeg, aria2 and python
+apt install wget -y
+apt install ffmpeg -y
+apt install python -y
+apt install aria2 -y
 
-# Install bashrc
-sh setup-bash.sh
+# Upgrade pip and install youtube-dl
+pip install --upgrade pip
+pip install youtube-dl
+
+cd scripts
+
+# dos2unix conversion for scripts
+dos2unix header
+dos2unix index
+dos2unix wastatus
+dos2unix wastatus-header
+dos2unix wastatus-video
+dos2unix wastatus-audio
+dos2unix download
+dos2unix dl-header
+
+# Copy custom scripts to /usr/bin
+cp header /data/data/com.termux/files/usr/bin
+cp index /data/data/com.termux/files/usr/bin
+cp wastatus /data/data/com.termux/files/usr/bin
+cp wastatus-header /data/data/com.termux/files/usr/bin
+cp wastatus-video /data/data/com.termux/files/usr/bin
+cp wastatus-audio /data/data/com.termux/files/usr/bin
+cp download /data/data/com.termux/files/usr/bin
+cp dl-header /data/data/com.termux/files/usr/bin
+
+# Set permission
+cd /data/data/com.termux/files/usr/bin
+chmod +x header
+chmod +x index
+chmod +x wastatus
+chmod +x wastatus-header
+chmod +x wastatus-video
+chmod +x wastatus-audio
+chmod +x download
+chmod +x dl-header
+
+cd ..
+
+# Load custom bash.bashrc
+cp bash.bashrc /data/data/com.termux/files/usr/etc
