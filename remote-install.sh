522623f00a90d356ed4167c31b602b20541193c9
Adithya S Sekhar <adithya0332@gmail.com>
2020-05-25 14:14:54 +0530

    Revert "remote-install: switch to master"
    This reverts commit ff8b12c9a01ba1923eaae21fe14f275d2a59a9a7.

--- remote-install.sh
+++ remote-install.sh
@@ -5,7 +5,7 @@
 mkdir my-first-repo >/dev/null 2>&1
 cd my-first-repo
 echo 'Getting files'
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/files_list.txt' -o files_list.txt >/dev/null 2>&1
+curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/testing/files_list.txt' -o files_list.txt >/dev/null 2>&1
 xargs -n 1 curl -O < files_list.txt
 echo ''
 echo 'Starting install.sh'
