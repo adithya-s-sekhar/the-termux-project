8b07262c282269f6ff3e1a06f6d3522c5bcc8a01
Adithya S Sekhar <adithya0332@gmail.com>
2020-05-25 14:09:53 +0530

    remote-install: Use files_list instead of hardcoded links

--- remote-install.sh
+++ remote-install.sh
@@ -5,17 +5,8 @@
 mkdir my-first-repo >/dev/null 2>&1
 cd my-first-repo
 echo 'Getting files'
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/aria2manager' -o aria2manager >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/banner' -o banner >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/index' -o index >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/install.sh' -o install.sh >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/newbash.bashrc' -o newbash.bashrc >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/uninstall.sh' -o uninstall.sh >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/wastatus' -o wastatus >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/wastatus-audio' -o wastatus-audio >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/wastatus-video' -o wastatus-video >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/wastatus-banner' -o wastatus-banner >/dev/null 2>&1
-curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/master/yt' -o yt >/dev/null 2>&1
+curl 'https://raw.githubusercontent.com/adithya-s-sekhar/my-first-repo/testing/files_list.txt' -o files_list.txt >/dev/null 2>&1
+xargs -n 1 curl -O < files_list.txt
 echo ''
 echo 'Starting install.sh'
 echo ''
